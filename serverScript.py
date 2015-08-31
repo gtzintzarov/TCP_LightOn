9bac6e8a81d71bba9fe7c550e27eecac5bf00154
unknown <gtzintzarov@gmail.com>
2015-08-31 00:08:30 -0400

    bug space fix server

--- serverScript.py
+++ serverScript.py
@@ -23,25 +23,24 @@ while 1:
 	conn, addr = s.accept()
 	#print 'Connected by', addr
 	#while 1:
-    data = conn.recv(1024)
+	data = conn.recv(1024)
 
     #Turn ALL off
-    if data == "all_off":
+	if data == "all_off":
 		#print repr(data)
-    	#	if not data: break
+   		#	if not data: break
 		#finalMessage = "{}".format(data)
-   		conn.sendall("Received: Turn ALL OFF")
+		conn.sendall("Received: Turn ALL OFF")
 		GPIO.output(greenPin, GPIO.LOW)
 		GPIO.output(yellowPin, GPIO.LOW)
 		GPIO.output(redPin, GPIO.LOW)
 		print ("Green: 0\nYellow: 0\nRed: 0\n")
-
     #Turn on/off green light
 	elif data == "green_on":
 		#print repr(data)
     	#	if not data: break
 		#finalMessage = "{}".format(data)
-   		conn.sendall("Received: Turn Green On")
+		conn.sendall("Received: Turn Green On")
 		GPIO.output(greenPin, GPIO.HIGH)
 		print ("Green: 1\nYellow: 0\nRed: 0\n")
 	elif data == "green_off":
@@ -49,13 +48,12 @@ while 1:
 		conn.sendall("Received: Turn Green Off")
 		GPIO.output(greenPin, GPIO.LOW)
 		print ("Green: 0\nYellow: 0\nRed: 0\n")
-
 	#Turn on/off yellow light
 	elif data == "yellow_on":
 		#print repr(data)
     	#	if not data: break
 		#finalMessage = "{}".format(data)
-   		conn.sendall("Received: Turn Yellow On")
+		conn.sendall("Received: Turn Yellow On")
 		GPIO.output(yellowPin, GPIO.HIGH)
 		print ("Green: 0\nYellow: 1\nRed: 0\n")
 	elif data == "yellow_off":
@@ -63,13 +61,12 @@ while 1:
 		conn.sendall("Received: Turn Yellow Off")
 		GPIO.output(yellowPin, GPIO.LOW)
 		print ("Green: 0\nYellow: 0\nRed: 0\n")	
-
 	#Turn on/off red light
 	elif data == "red_on":
 		#print repr(data)
     	#	if not data: break
 		#finalMessage = "{}".format(data)
-   		conn.sendall("Received: Turn Red On")
+		conn.sendall("Received: Turn Red On")
 		GPIO.output(redPin, GPIO.HIGH)
 		print ("Green: 0\nYellow: 0\nRed: 1\n")
 	elif data == "red_off":
