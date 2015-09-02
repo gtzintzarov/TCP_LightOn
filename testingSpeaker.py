abb9d5b3e7b1152a5e20a637a04105e291cef11f
George-Pi <gtzintzarov@gmail.com>
2015-09-02 04:27:56 +0000

    updated testing speakerwith all 3 notes

--- testingSpeaker.py
+++ testingSpeaker.py
@@ -1,11 +1,25 @@
 #! /usr/bin/python
 
 import RPi.GPIO as GPIO
+import time
+
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 GPIO.setup(18, GPIO.OUT)
 
-pwm = GPIO.PWM(18,440)
+pwm = GPIO.PWM(18,392)
 pwm.start(50)
+time.sleep(.2)
+pwm.stop()
+time.sleep(.2)
+pwm = GPIO.PWM(18,587)
+pwm.start(50)
+time.sleep(.2)
+pwm.stop()
+time.sleep(.2)
+pwm = GPIO.PWM(18,830)
+pwm.start(50)
+time.sleep(.6)
+pwm.stop()
