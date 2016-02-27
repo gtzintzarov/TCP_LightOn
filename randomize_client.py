f4d355dc95445b6ef161f7b3826ccd3fc5a5f5c4
George <gtzintzarov@gmail.com>
2016-02-26 22:56:27 -0500

    updating the library

--- randomize_client.py
+++ randomize_client.py
@@ -3,7 +3,7 @@
 # Echo client program
 import socket
 
-HOST = '50.155.99.140'    # The remote host
+HOST = '98.252.248.93'    # The remote host
 PORT = 50007              # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
